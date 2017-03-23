865cae4312b7bae9b56640a85a0f6e6b3b96f22e
Tony Tran <tcbtran@calvisitor-10-105-146-91.calvisitor.1918.berkeley.edu>
2017-03-22 20:24:00 -0700

    Second Branch Add

---
+++ assets/js/dine-in.js
@@ -0,0 +1,38 @@
+
+// function getRecipe(ingredients) {
+
+// These are not the API Keys you're looking for
+var APIkey = "2394e022160abd9e7a8ac3318b5e584e";
+
+// Ingredients are to be comma separated
+var ingredients = ["ground beef", "cheese", "lettuce"];
+
+// Sorting results parameter;  r by rating and t for trending.  Using rating for default.
+var sort = "r";
+
+var queryURL = "http://food2fork.com/api/search?key=" + APIkey + "&q=" + ingredients + "&sort=" + sort;
+// console.log(queryURL);
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(recipeResults) {
+
+    createHTML(recipeResults);
+
+    });
+
+  function createHTML(recipeResults) {
+
+    var rawTemplate = document.getElementById("handlebar-template").innerHTML;
+
+    var compiledTemplate = Handlebars.compile(rawTemplate);
+
+    var generatedHTML = compiledTemplate(JSON.parse(recipeResults));
+
+    var recipeContainer = document.getElementById("result");
+    recipeContainer.innerHTML = generatedHTML;
+
+  };
+
+// };
