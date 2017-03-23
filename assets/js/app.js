fa67b529fa92c03ae746c4cd61f8341341649a77
Andy Scott <andy@aoscott.com>
2017-03-22 19:29:04 -0700

    changes to js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,22 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function() {
+    console.log("ready!");
 
     $("button").click(function() {
 
     	var userInput = $(".form-control").val().trim();
-    		alert(userInput);
-    });
+    		//alert(userInput);
+
+    	var options = $(this).attr("data-option");
+			if (options === "dine-in") {
+				getYelp(userInput);
+			}
+
+		else if (options === "dine-out") {
+				getRecipe(userInput);
+			}
+
+		else {
+			console.log("ERROR!");
+			}
 });
